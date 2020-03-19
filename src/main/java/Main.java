8470078f56bc95926aac56000ac0897734bf0d0d
Betsara Marcellin <betsara.marcellin@etu.unice.fr>
2020-03-19 13:22:50 +0100

    #1 add skeleton for office cli

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import api.DroneDeliveryAPI;
+import cli.framework.Shell;
+
+public class Main {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+//        Shell s = new Shell(new DroneDeliveryAPI(host, port));
+//        s.register(
+//                Bye.class,
+//                Help.class,
+//                Startdelivery.class
+//        );
+//        s.run();
+    }
+
+}
---
+++ src/main/java/api/DroneDeliveryAPI.java
@@ -0,0 +1,21 @@
+package api;
+
+import javax.xml.ws.BindingProvider;
+import java.net.URL;
+
+public class DroneDeliveryAPI {
+//    public DeliveryService deliveries;
+
+    public DroneDeliveryAPI(String host, String port) {
+        initDeliveries(host, port);
+    }
+
+    public void initDeliveries(String host, String port)
+    {
+//        URL wsdlLocation = DroneDeliveryAPI.class.getResource("/DeliveryWS.wsdl");
+//        DeliveryServiceImplService factory = new DeliveryServiceImplService(wsdlLocation);
+//        this.deliveries = factory.getDeliveryServiceImplPort();
+//        String address = "http://" + host + ":" + port + "/drone-delivery-backend/webservices/DeliveryWS?wsdl";
+//        ((BindingProvider) deliveries).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, address);
+    }
+}
