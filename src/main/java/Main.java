d2b61d48a072b0e3ac4a0602b8d0e449b6a3c38d
Betsara Marcellin <betsara.marcellin@etu.unice.fr>
2020-03-19 20:51:47 +0100

    #1 change wsdl to knew one

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
     {
         String host = ( args.length == 0 ? "localhost" : args[0] );
         String port = ( args.length < 2  ? "8080"      : args[1] );
-        System.out.println("Starting Drone Delivery");
+        System.out.println("Starting Drone Delivery OFFICE");
         System.out.println("  - Remote server: " + host);
         System.out.println("  - Port number:   " + port);
         Shell s = new Shell(new DroneDeliveryAPI(host, port));
--- src/main/java/cli/commands/Scheduledelivery.java
+++ src/main/java/cli/commands/Scheduledelivery.java
@@ -1,16 +1,22 @@
 package cli.commands;
 
+import stubs.delivery.Exception_Exception;
+
 import java.util.List;
 
 public class Scheduledelivery extends Command {
     @Override
     public void execute(List<String> args) {
-//        if(args.size()<2) {
-//            System.err.println("No delivery number selected");
-//            return;
-//        }
-//        System.out.println(String.format("Scheduling delivery : %s for %s", args.get(1), args.get(0)));
-//        this.shell.getDroneDeliveryAPI().deliveries.scheduleDelivery(args.get(0), args.get(1));
+        if(args.size()<2) {
+            System.err.println("No delivery number selected");
+            return;
+        }
+        System.out.println(String.format("Scheduling delivery : %s for %s", args.get(1), args.get(0)));
+        try {
+            this.shell.getDroneDeliveryAPI().deliveries.scheduleDelivery(args.get(0), args.get(1));
+        } catch (Exception_Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("Delivery scheduled");
     }
 
