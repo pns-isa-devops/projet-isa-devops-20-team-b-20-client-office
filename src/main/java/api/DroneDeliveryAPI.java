16f11e40be1c2588f1b78a914b24e1c58a49ffc8
Alexandre Lon <alexandre.longordo@etu.univ-cotedazur.fr>
2020-03-27 00:51:53 +0100

    #1 Fix Error generating java from wsdl

--- src/main/java/api/DroneDeliveryAPI.java
+++ src/main/java/api/DroneDeliveryAPI.java
@@ -1,23 +1,22 @@
 package api;
 
-import stubs.delivery.DeliveryService;
-import stubs.delivery.DeliveryServiceImplService;
+import stubs.delivery.DeliveryScheduleService;
+import stubs.delivery.DeliveryScheduleServiceImplService;
 
 import javax.xml.ws.BindingProvider;
 import java.net.URL;
 
 public class DroneDeliveryAPI implements API {
-    public DeliveryService deliveries;
+    public DeliveryScheduleService deliveries;
 
     public DroneDeliveryAPI(String host, String port) {
         initDeliveries(host, port);
     }
 
-    public void initDeliveries(String host, String port)
-    {
+    public void initDeliveries(String host, String port) {
         URL wsdlLocation = DroneDeliveryAPI.class.getResource("/DeliveryWS.wsdl");
-        DeliveryServiceImplService factory = new DeliveryServiceImplService(wsdlLocation);
-        this.deliveries = factory.getDeliveryServiceImplPort();
+        DeliveryScheduleServiceImplService factory = new DeliveryScheduleServiceImplService(wsdlLocation);
+        this.deliveries = factory.getDeliveryScheduleServiceImplPort();
         String address = "http://" + host + ":" + port + "/drone-delivery-backend/webservices/DeliveryWS?wsdl";
         ((BindingProvider) deliveries).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, address);
     }
