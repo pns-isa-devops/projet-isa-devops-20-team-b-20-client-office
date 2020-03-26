9eb7f0f70f92a30f87b300deb623b4ef9c0c3279
Alexandre Lon <alexandre.longordo@etu.univ-cotedazur.fr>
2020-03-26 18:29:40 +0100

    #1 Fix some errors

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,20 +2,18 @@ import api.DroneDeliveryAPI;
 import cli.commands.Scheduledelivery;
 import cli.framework.Shell;
 
+import cli.commands.Bye;
+import cli.commands.Help;
+
 public class Main {
-    public static void main( String[] args ) throws InstantiationException, IllegalAccessException
-    {
-        String host = ( args.length == 0 ? "localhost" : args[0] );
-        String port = ( args.length < 2  ? "8080"      : args[1] );
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+        String host = (args.length == 0 ? "localhost" : args[0]);
+        String port = (args.length < 2 ? "8080" : args[1]);
         System.out.println("Starting Drone Delivery OFFICE");
         System.out.println("  - Remote server: " + host);
         System.out.println("  - Port number:   " + port);
         Shell s = new Shell(new DroneDeliveryAPI(host, port));
-        s.register(
-                Scheduledelivery.class
-                Bye.class
-                Help.class
-        );
+        s.register(Scheduledelivery.class, Bye.class, Help.class);
         s.run();
     }
 
--- src/main/java/cli/commands/Scheduledelivery.java
+++ src/main/java/cli/commands/Scheduledelivery.java
@@ -1,19 +1,19 @@
 package cli.commands;
 
 import stubs.delivery.Exception_Exception;
-
+import api.DroneDeliveryAPI;
 import java.util.List;
 
 public class Scheduledelivery extends Command {
     @Override
     public void execute(List<String> args) {
-        if(args.size()<2) {
+        if (args.size() < 2) {
             System.err.println("No delivery number selected");
             return;
         }
         System.out.println(String.format("Scheduling delivery : %s for %s", args.get(1), args.get(0)));
         try {
-            ((DroneDeliveryAPI)this.shell.getDroneDeliveryAPI()).deliveries.scheduleDelivery(args.get(0), args.get(1));
+            ((DroneDeliveryAPI) this.shell.getDroneDeliveryAPI()).deliveries.scheduleDelivery(args.get(0), args.get(1));
         } catch (Exception_Exception e) {
             e.printStackTrace();
         }
