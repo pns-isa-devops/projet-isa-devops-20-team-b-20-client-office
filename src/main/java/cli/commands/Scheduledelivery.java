dedd3b707d56232ffbc0c9783b487472b12ae84e
Betsara Marcellin <betsara.marcellin@etu.unice.fr>
2020-03-29 11:52:07 +0200

    projet-isa-devops-20-team-b-20 #39 update client office

--- src/main/java/cli/commands/Scheduledelivery.java
+++ src/main/java/cli/commands/Scheduledelivery.java
@@ -9,7 +9,8 @@ public class Scheduledelivery extends Command {
     @Override
     public void execute(List<String> args) {
         if (args.size() < 2) {
-            System.err.println("No delivery number selected");
+            System.err.println("Not enough arguments");
+            System.err.println("scheduledelivery <HH:mm> <deliveryID>");
             return;
         }
         System.out.println(String.format("Scheduling delivery : %s for %s", args.get(1), args.get(0)));
